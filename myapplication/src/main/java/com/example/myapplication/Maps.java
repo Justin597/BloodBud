fac6b0a1799bbf44cc5ba7291fcee166f30e3933
kyleornstein <kyle.ornstein@yahoo.com>
2018-04-23 01:55:05 -0400

    maps update

--- myapplication/src/main/java/com/example/myapplication/Maps.java
+++ myapplication/src/main/java/com/example/myapplication/Maps.java
@@ -9,12 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class Maps extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Maps extends MapsActivity {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         Button mProfileView = (Button) findViewById(R.id.profile);
         mProfileView.setOnClickListener(new View.OnClickListener() {
@@ -50,4 +61,12 @@ public class Maps extends AppCompatActivity {
 
     }
 
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(43.028627, -76.122447);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in CUSE"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
